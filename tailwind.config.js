2e0e5800b4f0caecc8d688aa95989d60d3b9ec81
OmniDvW <geoffrey.frioli@gmail.com>
2023-07-14 08:57:17 +0200

    start train challenge news homepage

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ['./index.html'],
   theme: {
-    extend: {},
+    colors: {
+      transparent: 'transparent',
+      'soft-orange': 'hsl(35, 77%, 62%)',
+      'soft-red': 'hsl(5, 85%, 63%)',
+      'off-white': 'hsl(36, 100%, 99%)',
+      'grayish-blue': 'hsl(233, 8%, 79%)',
+      'dark-grayish-blue': 'hsl(236, 13%, 42%)',
+      'very-dark-blue': 'hsl(240, 100%, 5%)',
+    },
+    extend: {
+      fontFamily: {
+        inter: "inter-regular",
+      },
+    },
   },
   plugins: [],
 }
